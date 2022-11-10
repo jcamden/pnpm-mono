23e8794b92ae257ea9c5b6c177946c1ed4de32b5
John Camden <jcamden@box.com>
2022-11-09 20:10:21 -0500

    tailwind

--- packages/ui/src/Button/Button.tsx
+++ packages/ui/src/Button/Button.tsx
@@ -1,1 +1,5 @@
-export const Button = ({ text }: { text?: string }) => <button>{text}</button>;
+// import "../styles.css";
+
+export const Button = ({ text }: { text?: string }) => (
+  <button className="text-red-500">{text}</button>
+);
